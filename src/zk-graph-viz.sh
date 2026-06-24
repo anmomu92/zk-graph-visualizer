2683a2739602cada439996301401b4ac997ecb26
Antonio Morán Muñoz <anmomu92@gmail.com>
2026-06-25 00:59:02 +0200

    fixed: system service related issues.

--- src/zk-graph-viz.sh
+++ src/zk-graph-viz.sh
@@ -5,20 +5,18 @@ WWW_DIR="$( dirname "$( dirname -- "$( readlink -f -- "${BASH_SOURCE[0]}" )" )" 
 DATA_DIR="${WWW_DIR}/data"
 
 # create data directory
-mkdir -p ${DATA_DIR}
+if [ ! -d "${DATA_DIR}" ]; then
+    mkdir -p "${DATA_DIR}"
+fi
 
-BROWSER="firefox --private-window"
+# Run server
+if [[ $( pgrep -f "python3 -m http.server" ) || $( pgrep -f "python3 -m ${WWW_DIR}" ) ]]; then
+	printf "\nZK-GRAPH-VISUALIZER is already running. Killing the process.\n\n"
+else
+	exec python3 -m http.server -d ${WWW_DIR}
+fi
 
 # Generate JSON files
 zk graph -f json > "${DATA_DIR}/graph.json"
 zk list -f json > "${DATA_DIR}/notes.json"
 
-# Run server
-if [[ $( pgrep -f "python3 -m http.server" ) || $( pgrep -f "python3 -m ${WWW_DIR}" ) ]]; then
-	printf "\nZK-GRAPH-VISUALIZER is already running. Go to 0.0.0.0:8000 in your browser and push the refresh button in the top bar\n\n"
-else
-	python3 -m http.server -d ${WWW_DIR} &
-fi
-
-# Open the browser
-# ${BROWSER} "http://127.0.0.1:8000/"
